6a613f4820453c90ba188530a2021c56dbc7571e
corneliuze <31808521+corneliuze@users.noreply.github.com>
2021-10-12 08:11:39 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! and welcome to my first Jenkins Job"); 
+        System.out.println("Hello, World, welcome to my first Jenkins Job...I'm Enjoying it. "); 
     }
 }
 
